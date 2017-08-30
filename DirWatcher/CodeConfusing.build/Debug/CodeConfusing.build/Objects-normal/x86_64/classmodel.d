306ab442a9e6d3a8b1fe24fc5e551101c497aae2
蛋黄派 <wangliangfmail@163.com>
2017-08-30 18:13:02 +0800

    删除Xcode工程文件，需要的话使用qmake生成

--- DirWatcher/CodeConfusing.build/Debug/CodeConfusing.build/Objects-normal/x86_64/classmodel.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/apple/code_confusing/FileChooser/DirWatcher/classmodel.cpp \
-  classmodel.h
--- DirWatcher/CodeConfusing.build/Debug/CodeConfusing.build/Objects-normal/x86_64/dummy.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/apple/Qt/5.9.1/clang_64/mkspecs/features/data/dummy.cpp
